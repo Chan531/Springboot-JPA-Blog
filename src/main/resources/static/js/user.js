9f6546f02094c49675a907b24dfc919b1b849cc0
Chan531 <chan657312@gmail.com>
2022-10-07 11:21:49 +0900

    2022.10.07 중간 저장

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,10 @@ let index = {
         $("#btn-save").on("click", ()=>{ // function(){}, ()=>{} this를 바인딩하기 위해서!!
             this.save();
         });
+
+        $("#btn-login").on("click", ()=>{ // function(){}, ()=>{} this를 바인딩하기 위해서!!
+            this.login();
+        });
     },
 
     save: function(){
@@ -21,7 +25,7 @@ let index = {
         $.ajax({
             // 회원가입 수행 요청
             type:"POST",
-            url:"/blog/api/user",
+            url:"/api/user",
             data:JSON.stringify(data), // http body 데이터
             contentType:"application/json; charset=utf-8", // body 데이터가 어떤 타입인지 (MIME)
             dataType:"json" // 요청을 서버로 해서 응답이 왔을 때 기본적으로 (거의) 모든 것이 문자열인데 생긴게 JSON이라면 => javascript 오브젝트로 변경 
@@ -29,7 +33,32 @@ let index = {
             // 성공 시
             alert("회원가입이 완료되었습니다.");
             // console.log(resp);
-            location.href = "/blog";
+            location.href = "/";
+        }).fail(function(error){
+            // 실패 시
+            alert(JOSN.stringify(error));
+        }); 
+    },
+
+    login: function(){
+        // alert('user의 save 함수 호출됨');
+        let data = {
+            username: $("#username").val(),
+            password: $("#password").val()
+        };
+
+
+        $.ajax({
+            // 회원가입 수행 요청
+            type:"POST",
+            url:"/api/user/login",
+            data:JSON.stringify(data), // http body 데이터
+            contentType:"application/json; charset=utf-8", // body 데이터가 어떤 타입인지 (MIME)
+            dataType:"json" // 요청을 서버로 해서 응답이 왔을 때 기본적으로 (거의) 모든 것이 문자열인데 생긴게 JSON이라면 => javascript 오브젝트로 변경 
+        }).done(function(resp){
+            // 성공 시
+            alert("로그인이 완료되었습니다.");
+            location.href = "/";
         }).fail(function(error){
             // 실패 시
             alert(JOSN.stringify(error));
